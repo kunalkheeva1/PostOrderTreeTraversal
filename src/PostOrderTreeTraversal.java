d398b3d5db7b5bcf623212fd4401228983676b0f
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-16 20:51:11 -0700

    created a recursive method to call it in original method

--- src/PostOrderTreeTraversal.java
+++ src/PostOrderTreeTraversal.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 
 class TreeNode{
     int val;
@@ -15,6 +15,14 @@ class TreeNode{
 
 public class PostOrderTreeTraversal {
 
+
+    public static void rec(TreeNode root, ArrayList<Integer> list){
+        if(root == null) return;
+
+        rec(root.left, list);
+        rec(root.right, list);
+        list.add(root.val);
+    }
 
     public static void main(String[] args) {
 
