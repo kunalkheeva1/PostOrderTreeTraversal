e00a10ae6fc45f1d69f05988cab7ac40558bb2ce
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-16 20:47:25 -0700

    created a TreeNode method which stores value, right and left of the tree

--- src/PostOrderTreeTraversal.java
+++ src/PostOrderTreeTraversal.java
@@ -1,5 +1,22 @@
+
+
+class TreeNode{
+    int val;
+    TreeNode right;
+    TreeNode left;
+    TreeNode(int val, TreeNode right, TreeNode left){
+        this.val = val;
+        this.right = right;
+        this.left = left;
+    }
+}
+
+
+
 public class PostOrderTreeTraversal {
+
+
     public static void main(String[] args) {
-        
+
     }
 }
